25bb14884ca6601d660fb0620a9ebc5d6a283349
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-03-05 17:46:32 +0300

    Fix stop bug in 28byj

--- ultrasonicI2C/Motor28BYJ.cpp
+++ ultrasonicI2C/Motor28BYJ.cpp
@@ -13,9 +13,10 @@ Motor28BYJ::Motor28BYJ(uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4) {
 }
 
 void Motor28BYJ::step(int direction) {
+  if (direction == 0)
+    return;
   for (int pin = 0; pin < 4; pin++) {
-    if (direction != 0)
-      digitalWrite(_pins[pin], _sequence[(int)(3.5 - (3.5 * direction) + (direction * position))][pin]);
+    digitalWrite(_pins[pin], _sequence[(int)(3.5 - (3.5 * direction) + (direction * position))][pin]);
   }
   position ++;
   if (position > 7)
