8474212a844f299ef4a6a38ada0d8cfb152f5b49
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-06-29 18:30:36 +0300

    Reorganize files storage

--- tools/esp8266config/esp8266config.ino
+++ tools/esp8266config/esp8266config.ino
@@ -38,7 +38,7 @@ void setup() {
   Serial.println("begin setup");
   EEPROM.begin(512);
   // ssid, password, host, url
-  char data[4][32] = {"robohub", "robohub1", "192.168.33.2", "bug3"};
+  char data[4][32] = {"cyberhub", "cyberpass497", "192.168.100.4", "esp8266"};
   Serial.println("begin cicle");
   for (int i = 0; i < 4; i ++) {
     writeConfig(i, data[i]);
